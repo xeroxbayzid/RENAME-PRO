b5f798e6ea98d4eadb9707e8691572f3ddedee49
xeroxbayzid <134350713+xeroxbayzid@users.noreply.github.com>
2024-11-10 23:32:16 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8030394812:AAGBwzL6qfDhWeAMt9HZvGrlPmo1R0yVuh4")
+API_ID = int(os.environ.get("API_ID", "25751497"))
+API_HASH = os.environ.get("API_HASH", "c18db31105b92bba3cfa1ab685c4787f")
+OWNER = int(os.environ.get("OWNER", "5548923721"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "TestBayzidBot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002172515270"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://bayzidpremium:bayzidpremium@cluster0.dm24s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
